cec1fd9d9f60795af56d7519bb35b02ff1740a7d
Slavina Stefanova <sstefanova@wikimedia.org>
2024-05-23 11:19:14 +0200

    first db table

--- toolhunt/main.py
+++ toolhunt/main.py
@@ -1,9 +1,20 @@
+import os
+
 from fastapi import FastAPI, Depends
+from tortoise.contrib.fastapi import register_tortoise
 
 from toolhunt.config import get_settings, Settings
 
 
 app = FastAPI()
+
+register_tortoise(
+    app,
+    db_url=os.getenv("DATABASE_URL"),
+    modules={"models": ["toolhunt.models.tortoise"]},
+    generate_schemas=True,
+    add_exception_handlers=True,
+)
 
 
 @app.get("/ping")
---
+++ toolhunt/models/tortoise.py
@@ -0,0 +1,15 @@
+from tortoise import fields, models
+
+class Tool(models.Model):
+    name = fields.CharField(max_length=255, pk=True)
+    title = fields.CharField(max_length=255)
+    description = fields.TextField()
+    url = fields.CharField(max_length=2047)
+    last_updated = fields.DatetimeField()
+
+    # tasks: fields.ReverseRelation["Task"]
+
+    class Meta:
+        table = "tool"
+        charset = "binary"
+
