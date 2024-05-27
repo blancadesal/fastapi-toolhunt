9fea343dc34c94dad5193e10d40be80c36d519a6
Slavina Stefanova <sstefanova@wikimedia.org>
2024-05-27 09:05:16 +0200

    logging

--- toolhunt/api/utils.py
+++ toolhunt/api/utils.py
@@ -1,5 +1,18 @@
+import logging
+
 import httpx
 from fastapi import HTTPException
+
+
+logging.basicConfig(
+    filename="db_update.log",
+    format="%(asctime)s:%(levelname)s:%(message)s",
+    filemode="w",
+    encoding="utf-8",
+    level=logging.INFO,
+)
+
+logger = logging.getLogger()
 
 
 class ToolhubClient:
