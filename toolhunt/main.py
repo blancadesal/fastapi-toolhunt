9f8623371819d8ae6a60062a4ed4c7dc5b84e9b0
Slavina Stefanova <sstefanova@wikimedia.org>
2024-05-28 13:50:27 +0200

    fix db init

--- toolhunt/main.py
+++ toolhunt/main.py
@@ -1,25 +1,40 @@
 import logging
+import os
 from contextlib import asynccontextmanager
+from typing import AsyncGenerator
 
 from fastapi import FastAPI
 
-from toolhunt.api import ping
-from toolhunt.db import init_db
+# from toolhunt.db import init_db
+from tortoise.contrib.fastapi import RegisterTortoise
+
+from toolhunt.api import ping, tasks
 
 log = logging.getLogger("uvicorn")
 
+API_PREFIX = "/api/v1"
+
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
-    log.info("Starting up...")
-    init_db(app)
-    yield
-    log.info("Shutting down...")
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    # app startup
+    async with RegisterTortoise(
+        app,
+        db_url=os.getenv("DATABASE_URL"),
+        modules={"models": ["toolhunt.models.tortoise"]},
+        generate_schemas=False,
+        add_exception_handlers=True,
+    ):
+        # db connected
+        yield
+        # app teardown
+    # db connections closed
 
 
 def create_app() -> FastAPI:
     app = FastAPI(lifespan=lifespan)
-    app.include_router(ping.router, prefix="/api/v1")
+    app.include_router(ping.router, prefix=API_PREFIX)
+    app.include_router(tasks.router, prefix=API_PREFIX, tags=["tasks"])
     return app
 
 
