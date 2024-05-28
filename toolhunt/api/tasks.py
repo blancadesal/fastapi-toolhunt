a62df962f5cfdf049d830601d91c9c0f5979106b
Slavina Stefanova <sstefanova@wikimedia.org>
2024-05-28 13:49:45 +0200

    tasks endpoint

---
+++ toolhunt/api/tasks.py
@@ -0,0 +1,37 @@
+import random
+from typing import List
+
+from fastapi import APIRouter, HTTPException
+from tortoise.contrib.fastapi import HTTPNotFoundError
+
+from toolhunt.models.pydantic import TaskSchema
+from toolhunt.models.tortoise import Task
+
+router = APIRouter()
+
+
+async def get_tasks_from_db() -> List[TaskSchema]:
+    tasks = await Task.all().prefetch_related("tool_name", "field_name")
+    random_tasks = random.sample(tasks, min(len(tasks), 10))
+    return [
+        TaskSchema(
+            id=task.id,
+            tool_name=task.tool_name.name,
+            tool=task.tool_name,
+            field_name=task.field_name.name,
+            field=task.field_name,
+        )
+        for task in random_tasks
+    ]
+
+
+@router.get(
+    "/tasks",
+    response_model=List[TaskSchema],
+    responses={404: {"model": HTTPNotFoundError}},
+)
+async def get_tasks():
+    tasks = await get_tasks_from_db()
+    if not tasks:
+        raise HTTPException(status_code=404, detail="No tasks found")
+    return tasks
