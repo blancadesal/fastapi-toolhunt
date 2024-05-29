05be606bc7a2538cb5d27062fe882f9725475070
Slavina Stefanova <sstefanova@wikimedia.org>
2024-05-29 12:10:28 +0200

    only serve tasks not attempted in last 24h

--- toolhunt/api/tasks.py
+++ toolhunt/api/tasks.py
@@ -1,13 +1,18 @@
 import random
+from datetime import datetime, timedelta
 from typing import List, Optional
 
 from fastapi import APIRouter, HTTPException, Query
 from tortoise.contrib.fastapi import HTTPNotFoundError
+from tortoise.expressions import Q
 
+from toolhunt.config import get_settings
 from toolhunt.models.pydantic import TaskSchema
 from toolhunt.models.tortoise import Task
 
 router = APIRouter()
+
+settings = get_settings()
 
 
 # CRUD
@@ -21,8 +26,22 @@ async def get_tasks_from_db(
     if tool_name:
         query = query.filter(tool_name=tool_name)
 
+    if settings.environment != "dev":
+        # Filter out tasks attempted in the last 24 hours
+        twenty_four_hours_ago = datetime.now() - timedelta(hours=24)
+        query = query.filter(
+            Q(last_attempted__isnull=True) | Q(last_attempted__lt=twenty_four_hours_ago)
+        )
+
     tasks = await query
     random_tasks = random.sample(tasks, min(len(tasks), 10))
+
+    if settings.environment != "dev":
+        # Update last_attempted field
+        for task in random_tasks:
+            task.last_attempted = datetime.now()
+            await task.save()
+
     return [
         TaskSchema(id=task.id, tool=task.tool_name, field=task.field_name)
         for task in random_tasks
@@ -30,12 +49,18 @@ async def get_tasks_from_db(
 
 
 async def get_task_from_db(task_id: int) -> Optional[TaskSchema]:
-    task = (
-        await Task.filter(id=task_id)
-        .prefetch_related("tool_name", "field_name")
-        .first()
-    )
-    if task:
+    twenty_four_hours_ago = datetime.now() - timedelta(hours=24)
+    query = Task.filter(id=task_id)
+
+    if settings.environment != "dev":
+        query = query.filter(
+            Q(last_attempted__isnull=True) | Q(last_attempted__lt=twenty_four_hours_ago)
+        )
+
+    task = await query.first()
+    if task and settings.environment != "dev":
+        task.last_attempted = datetime.now()
+        await task.save()
         return TaskSchema(id=task.id, tool=task.tool_name, field=task.field_name)
     return None
 
